b046c8635ac175cd3674bc5a870e8114471644f3
prachi-santoshkadam-epam <prachi_santoshkadam@epam.com>
2026-06-05 10:57:23 +0000

    Add comment to main.tsx for testing commit and push functionality

--- octofit-tracker/frontend/src/main.tsx
+++ octofit-tracker/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+//new comment is added in this file to test the commit and push functionality of git
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 );
